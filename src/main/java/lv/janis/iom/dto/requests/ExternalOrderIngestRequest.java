25469fa1e64c870da71fe3d10d02d13fec4afe02
janis-karklins88 <janis.karklins88@gmail.com>
2026-01-20 12:28:53 +0200

    minor fix

--- src/main/java/lv/janis/iom/dto/requests/ExternalOrderIngestRequest.java
+++ src/main/java/lv/janis/iom/dto/requests/ExternalOrderIngestRequest.java
@@ -3,6 +3,7 @@ package lv.janis.iom.dto.requests;
 import java.util.List;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lv.janis.iom.enums.ExternalOrderSource;
